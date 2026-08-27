4b8aa13ec1e15bd6520c7629a6e6eecaf7876582
Pitiedwzr <43533746+Pitiedwzr@users.noreply.github.com>
2026-08-28 02:38:11 +1200

    fix: incorrect method

--- app/routers/collection.py
+++ app/routers/collection.py
@@ -192,7 +192,7 @@ async def modify_collection(
     return {"code": 114514, "message": "Collection modified"}
 
 
-@router.post("/{idStr}/diff")
+@router.get("/{idStr}/diff")
 async def get_collection_diff(
     idStr: str,
     db: AsyncSession = Depends(get_db)
